ec495e99b3336abe20ed9e110f74e5cd50a0bf8a
FernandoKuo <101562764+FernandoKuo@users.noreply.github.com>
2024-04-16 15:45:55 -0300

    Add files via upload

---
+++ cocina/cocina.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap demo</title>
+<link rel="stylesheet" href="c_styles.css" />
+</head>
+<!--PHP for chefs-->
+<?php
+
+?>
+<body>
+<div style="border-left:5px solid #000;height:80%;position:absolute"></div>
+<div style="border-left:5px solid #000;height:80%;position:absolute;left:30%"></div>
+<div style="border-left:5px solid #000;height:80%;position:absolute;left:69.5%"></div>
+<div style="border-left:5px solid #000;height:80%;position:absolute;left:99%"></div>
+
+<table><tr>
+  <th>PASTAS</th>
+  <th>PLATOS PRINCIPALES</th>
+  <th>POSTRES</th>
+</tr></table>
+<table class="t2"><tr>
+<td>
+  <div><button>a</button>x4</div>
+</td>
+<td>
+  <div><button>a</button>x4</div>
+</td>
+<td>
+  <div><button>a</button>x4</div>
+</td>
+</tr></table>
+
+<div class="des">test</div>
+</body>
+</html>
